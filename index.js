b6f0e5349c3f0b2c692ff84d7c7c9a9aaf742265
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-03 21:56:55 +0600

    first class functions completed

---
+++ index.js
@@ -0,0 +1,20 @@
+// receivesAFunction 
+function receivesAFunction (callback){
+callback();
+}
+
+// returnsANamedFunction
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I am a named function!");
+  }
+  return namedFunction;
+}
+
+
+// returnsAnAnonymousFunction 
+function returnsAnAnonymousFunction (){
+    return function (){
+        console.log("I am annonymous function")
+    }
+}
